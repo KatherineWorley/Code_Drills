ba2fb945ea5d086379f7603b788a540f633a1a06
Katherine Worley <kdesign216@yahoo.com>
2018-02-11 07:35:37 -0500

    added an iteration ruby file

---
+++ iterate.rb
@@ -0,0 +1,22 @@
+
+############################################ Iterate Color
+
+color = [
+  "blue",
+  "purple",
+  "pink",
+  "yellow"
+]
+
+eye_color = [
+  "brown",
+  "purple",
+  "pink",
+  "hazel",
+  "green",
+  "yellow"
+]
+
+panel = [color-(color-eye_color)]
+
+puts panel
