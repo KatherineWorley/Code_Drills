90bfd2615984db7485cf145067d006015490827d
Katherine Worley <kdesign216@yahoo.com>
2018-02-12 07:45:54 -0500

    drills for different ways to fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,4 @@
-    1.upto(100) do |i|
+    1.upto(10) do |i|
       if i % 5 == 0 and i % 3 == 0
         puts "FizzBuzz"
       elsif i % 5 == 0
@@ -10,8 +10,21 @@
       end
     end
 
+    1.upto(10) do |i|
+      if i % 5 == 0 and i % 3 == 0
+        puts "YoYo"
+      elsif i % 5 == 0
+        puts "HoHo"
+      elsif i % 3 == 0
+        puts "MoMo"
+      else 
+        puts i 
+      end 
+    end 
+        
 
-(1..100).each{|i| 
+
+(1..10).each{|i| 
     x = ''
     x += 'Fizz' if i%3==0
     x += 'Buzz' if i%5==0
@@ -19,12 +32,59 @@
 }
 
 
-def piglatin(word)
-  if word =~ (/\A[aeiou]/i)
-    word = word + 'ay'
-  elsif word =~ (/\A[^aeiou]/i)
-    match = /\A[^aeiou]/i.match(word)
-    word = match.post_match + match.to_s + 'ay'
+
+
+def fizzbuzz(number)
+  number.times do |i|
+    if  i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 5 == 0
+      puts "Buzz"
+     elsif i % 3 == 0
+      puts "Fizz"
+    else
+     puts i
+     
+    end
   end
-  word
+
 end
+
+puts fizzbuzz(20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def piglatin(word)
+#   if word =~ (/\A[aeiou]/i)
+#     word = word + 'ay'
+#   elsif word =~ (/\A[^aeiou]/i)
+#     match = /\A[^aeiou]/i.match(word)
+#     word = match.post_match + match.to_s + 'ay'
+#   end
+#   word
+# end
+
+# puts piglatin(people)
