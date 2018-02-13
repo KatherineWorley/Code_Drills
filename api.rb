939da0b7b1922bbb8b4a47aad6b4e0682aa0ddb2
Katherine Worley <kdesign216@yahoo.com>
2018-02-13 11:52:31 -0500

    updated api and css file

---
+++ api.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+require	'sinatra'
+
+get '/:zip_code' do
+	zip_code = params['zip_code']
+	puts zip_code
+	key = 'LONG KEY INSERTED HERE FROM OPEN WEATHER MAP'
+
+	url = "http://api.openweathermap.org/data/2.5/weather?zip=#{zip_code},us&appid=#{key}"
+
+	response = HTTParty.get(url).parsed_response
+	kelvin_temp = response['main']['temp']
+	f_temp = ((kelvin_temp * 9/5) - 459.67).round
+	description = response['weather'][0]['description']
+	city_name = response['name']
+	wind_speed = response['wind']['speed']
+	wind_direction = response['wind']['deg']
+
+	"For #{city_name}, it is currently #{f_temp}° F, with #{description}. That wind is blowing #{wind_speed}MPG at #{wind_direction}°"
+
+end 
